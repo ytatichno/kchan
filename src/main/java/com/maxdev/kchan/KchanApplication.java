130b2c987c530b11b2d7e28f3e5df55c63dadcfd
ytatichno <ytatichno@gmail.com>
2024-03-03 21:04:19 +0300

    Initial commit

---
+++ src/main/java/com/maxdev/kchan/KchanApplication.java
@@ -0,0 +1,13 @@
+package com.maxdev.kchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KchanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KchanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxdev/kchan/KchanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxdev.kchan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KchanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
