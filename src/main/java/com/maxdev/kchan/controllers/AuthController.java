3ce730290906d63c9cf19e35b79e7340367beb6c
ytatichno <ytatichno@gmail.com>
2024-04-18 22:19:52 +0300

    add AuthController for login and signup

---
+++ src/main/java/com/maxdev/kchan/controllers/AuthController.java
@@ -0,0 +1,135 @@
+package com.maxdev.kchan.controllers;
+
+import com.maxdev.kchan.models.Credential;
+import com.maxdev.kchan.models.Usercard;
+import com.maxdev.kchan.repo.CredentialsRepository;
+import com.maxdev.kchan.repo.UsercardsRepository;
+import com.maxdev.kchan.security.CredentialsService;
+import com.maxdev.kchan.security.SaltApplier;
+import com.maxdev.kchan.security.SimpleSaltApplier;
+import com.maxdev.kchan.security.filters.CookieAuthFilter;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.HashMap;
+
+/**
+ * Created by ytati
+ * on 23.03.2024.
+ */
+@RestController
+@RequestMapping("/rest/auth")
+@Slf4j
+public class AuthController {
+    //    private AuthenticationManager authenticationManager;
+    private UsercardsRepository ur;
+    private CredentialsRepository cr;
+    private PasswordEncoder passwordEncoder;
+    private SaltApplier saltApplier;
+    private CredentialsService credentialsService;
+
+    @Autowired
+    public AuthController(UsercardsRepository ur, CredentialsRepository cr, PasswordEncoder passwordEncoder, SaltApplier saltApplier, CredentialsService credentialsService) {
+        this.ur = ur;
+        this.cr = cr;
+        this.passwordEncoder = passwordEncoder;
+        this.saltApplier = saltApplier;
+        this.credentialsService = credentialsService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@AuthenticationPrincipal Credential user,
+                                   HttpServletResponse servletResponse) {
+
+        log.warn("user login: " + user.getUsercard().getNick());
+
+        Cookie authCookie = getAuthCookie(user);
+
+        servletResponse.addCookie(authCookie);
+
+        return ResponseEntity.ok("Successfully logged in");
+    }
+
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(@AuthenticationPrincipal Credential user) {
+        log.warn("user logout: " + user.getUsercard().getNick());
+
+        SecurityContextHolder.clearContext();
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody HashMap<String, String> body,
+                                    HttpServletResponse servletResponse) {
+
+        String email = (String) body.get("email");
+        String nick = (String) body.get("nickname");
+        String pwd = (String) body.get("password");
+
+        log.warn("user signup: " + email + ", " + nick);
+
+        if (cr.existsByEmail(email))
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Email already taken");
+        if (ur.existsByNick(nick))
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Nickname already taken");
+        if (!checkPasswordRobustness(pwd))
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Password too weak, try another");
+
+        // register usercard
+        Usercard usercard = new Usercard();
+        usercard.setNick(nick);
+        ur.save(usercard);
+
+        // register credential
+        Credential credential = new Credential();
+        credential.setEmail(email);
+        credential.setSalt("" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        credential.setPwd(
+                passwordEncoder.encode(
+                        saltApplier.applySalt(
+                                pwd,
+                                credential.getSalt(),
+                                SimpleSaltApplier.SimpleSaltMode.DUPLICATE_TO_BOTH_ENDS.ordinal()
+                        )
+                )
+        );
+        // link usercatd to credential
+        credential.setUsercard(usercard);
+        cr.save(credential);
+
+        Cookie authCookie = getAuthCookie(credential);
+        servletResponse.addCookie(authCookie);
+        return ResponseEntity.ok("Successfully signed up");
+    }
+
+    @NotNull
+    private Cookie getAuthCookie(Credential user) {
+        Cookie authCookie = new Cookie(CookieAuthFilter.AUTH_COOKIE_NAME, credentialsService.createToken(user));
+        authCookie.setHttpOnly(true);
+        authCookie.setSecure(true);
+        authCookie.setMaxAge((int) Duration.of(90, ChronoUnit.SECONDS).toSeconds());
+        authCookie.setPath("/");
+        return authCookie;
+    }
+
+    private boolean checkPasswordRobustness(String pwd) {
+        return pwd.length() >= 4;
+    }
+
+}
